e133b5ee9b859eabe76620f5a15b442abb8c25c0
AlexanderSahle <alexandersahle@gmail.com>
2024-03-14 20:01:24 -0700

    Intro SQL

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,9 +1,3 @@
---uses the hbtn_0d_tvshows database to list all generes of the show Dexter
---uses a database to list all rows in a table corresponding to all rows in another
-SELECT name
-FROM tv_generes
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = 'Dexter'
-GROUP BY name
-ORDER BY name ASC:
+-- Computes the average score of all records in the table second_table in my MySQL server.
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
