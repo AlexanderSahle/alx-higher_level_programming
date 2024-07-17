84102a2b8d4a6dbffe5185bfae63cbb179106d4a
AlexanderSahle <alexandersahle@gmail.com>
2024-07-16 21:36:52 -0700

    Update_Inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,6 +2,7 @@
 '''Module for Rectangle class.'''
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     '''A subclass representing a rectangle.'''
     def __init__(self, width, height):
